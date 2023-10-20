a788dbc92a0d7d5edd8355d4110cfbe3764b795c
Agnel Lazar Alappat <59665263+agnellazar@users.noreply.github.com>
2023-10-20 21:49:39 +0530

    remaining and expense final

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,1 +1,19 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+const ExpenseTotal = () => {
+    const { budget, expenses, currency } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
 
+    if(budget < totalExpenses) {
+        alert(' You cannot reduce budget below spending')
+    }
+
+    return (
+        <div className='alert alert-primary'>
+            <span>Spent so far: {currency}{totalExpenses}</span>
+        </div>
+    );
+};
+export default ExpenseTotal;
